37d8ec7967278fbbf524e1352fb7f2c5b4546de4
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-12-29 17:33:00 +0900

    [modify] propsで渡したstateをアップデートするテスト

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -4,12 +4,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { title: "Welcome" };
+  }
+
   render() {
-    const title = "Welcome Will!";
+    setTimeout(() => {
+      this.setState({ title: "Welcome Will!" })
+    }, 2000);
 
     return (
       <div>
-        <Header title={title} />
+        <Header title={this.state.title} />
         <Header title={"Other title"} />
         <Footer />
       </div>
