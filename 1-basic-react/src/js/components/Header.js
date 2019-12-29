e9520247bf63c45728183b6a1848aef7c1293e2c
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-12-29 17:37:21 +0900

    [modify] public class fields syntaxを使ってリファクタリング

--- 1-basic-react/src/js/components/Header.js
+++ 1-basic-react/src/js/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 import Title from "./Header/Title";
 
 export default class Header extends React.Component {
-  handleChange(e) {
+  handleChange = (e) => {
     const title = e.target.value;
     this.props.changeTitle(title);
   }
@@ -12,7 +12,7 @@ export default class Header extends React.Component {
     return (
       <div>
         <Title title={this.props.title} />
-        <input value={this.props.title} onChange={this.handleChange.bind(this)} />
+        <input value={this.props.title} onChange={this.handleChange} />
       </div>
     );
   }
--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -4,19 +4,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = { title: "Welcome" };
-  }
+  state = { title: "Welcome" };
 
-  changeTitle(title) {
+  changeTitle = (title) => {
     this.setState({title});
   }
 
   render() {
     return (
       <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+        <Header changeTitle={this.changeTitle} title={this.state.title} />
         <Footer />
       </div>
     );
