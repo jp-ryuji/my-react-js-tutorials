64c4f178a613d8b10948c535e04cc340bbb173b7
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-12-29 17:28:46 +0900

    [modify] DOMだけが更新されることを確認

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -4,9 +4,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "Will" };
+  }
+
   render() {
+    setTimeout(
+      () => { this.setState({ name: "Bob" }); }
+    , 1000);
+
     return (
       <div>
+        {this.state.name}
         <Header />
         <Footer />
       </div>
